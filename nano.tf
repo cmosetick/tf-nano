7de24124b963e5ab7a3b7f76044f1ada55552ccb
Chris Mosetick <cmosetick@gmail.com>
2016-04-21 09:37:44 -0700

    update nano.tf with latest changes

--- nano.tf
+++ nano.tf
@@ -5,40 +5,45 @@ provider "aws" {
 }
 resource "aws_instance" "Cmosetick-Nano" {
           ami = "${lookup (var.my_amis,var.region) }"
-                instance_type = "t2.nano"
-                key_name = "${var.key_name}" # required for terraform provisioner
-                subnet_id = "${aws_subnet.Cmosetick-Public.id}"
-								#subnet_id = "${var.my_subnet}"
-                availability_zone = "${var.my_availability_zone}"
-                count = "1"
-                security_groups = ["${aws_security_group.Cmosetick-Public.id}"]
-                # public ip required to provision with Terraform
-                associate_public_ip_address = "True"
-								tags {
-                        Name = "Cmosetick-Nano"
+              instance_type = "t2.nano"
+              key_name = "${var.key_name}" # required for terraform provisioner
+              subnet_id = "${aws_subnet.Cmosetick-Public.id}"
+							#subnet_id = "${var.my_subnet}"
+              availability_zone = "${var.my_availability_zone}"
+              count = "1"
+              security_groups = ["${aws_security_group.Cmosetick-Public.id}"]
+              # public ip required to provision with Terraform
+              associate_public_ip_address = "True"
+							tags {
+                Name = "Cmosetick-Nano"
                 }
-								# Copy distelli.yml file to /etc/disetelli.yml on remote system
-								provisioner "file" {
-								source = "files/distelli.yml"
-								destination = "/home/ubuntu/distelli.yml" # credentials file for distelli agent
-								connection {
-								user = "ubuntu"
-								agent = "True" # this is to use ssh-agent session for connection
-								}
+							provisioner "file" {
+							connection {
+					    user = "ubuntu"
+					    host = "${aws_instance.Cmosetick-Nano.public_ip}"
+					    timeout = "2m"
+					    key_file = "files/cmosetick-terraform"
+					    }
+							source = "files/doit.sh"
+							destination = "/home/ubuntu/test.sh"
 							}
-								provisioner "file" {
-								source = "files/client"
-								destination = "/home/ubuntu/client" # this is installer shell script for distelli agent
-								connection {
-								user = "ubuntu"
-								agent = "True" # this is to use ssh-agent session for connection
-								}
+							provisioner "file" {
+							connection {
+							user = "ubuntu"
+							host = "${aws_instance.Cmosetick-Nano.public_ip}"
+					    timeout = "2m"
+					    key_file = "files/cmosetick-terraform"
 							}
-								# Install distelli agent on remote system
-								provisioner "remote-exec" {
-								    inline = [
-										"curl -sSL https://www.distelli.com/download/client | sh",
-								    "sudo /usr/local/bin/distelli agent install -conf /home/ubuntu/distelli.yml"
-								    ]
-								}
+							source = "files/distelli.yml"
+							destination = "/home/ubuntu/distelli.yml"
+							}
+				provisioner "remote-exec" {
+        connection {
+        user = "ubuntu"
+        host = "${aws_instance.Cmosetick-Nano.public_ip}"
+        timeout = "2m"
+        key_file = "files/cmosetick-terraform"
+    	}
+      	script = "files/doit.sh"
+    }
 }
