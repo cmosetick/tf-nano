eec88b837fe34ff496f7ee24751fe67437952079
Chris Mosetick <cmosetick@gmail.com>
2016-04-21 21:21:43 -0700

    major overhaul to nano.tf - remove built in provisioner - add user-data parameter with cloud-config template

--- nano.tf
+++ nano.tf
@@ -4,46 +4,20 @@ provider "aws" {
 	region = "${var.region}"
 }
 resource "aws_instance" "Cmosetick-Nano" {
-          ami = "${lookup (var.my_amis,var.region) }"
-              instance_type = "t2.nano"
-              key_name = "${var.key_name}" # required for terraform provisioner
-              subnet_id = "${aws_subnet.Cmosetick-Public.id}"
-							#subnet_id = "${var.my_subnet}"
-              availability_zone = "${var.my_availability_zone}"
-              count = "1"
-              security_groups = ["${aws_security_group.Cmosetick-Public.id}"]
-              # public ip required to provision with Terraform
-              associate_public_ip_address = "True"
-							tags {
-                Name = "Cmosetick-Nano"
-                }
-							provisioner "file" {
-							connection {
-					    user = "ubuntu"
-					    host = "${aws_instance.Cmosetick-Nano.public_ip}"
-					    timeout = "2m"
-					    key_file = "files/cmosetick-terraform"
-					    }
-							source = "files/doit.sh"
-							destination = "/home/ubuntu/test.sh"
-							}
-							provisioner "file" {
-							connection {
-							user = "ubuntu"
-							host = "${aws_instance.Cmosetick-Nano.public_ip}"
-					    timeout = "2m"
-					    key_file = "files/cmosetick-terraform"
-							}
-							source = "files/distelli.yml"
-							destination = "/home/ubuntu/distelli.yml"
-							}
-				provisioner "remote-exec" {
-        connection {
-        user = "ubuntu"
-        host = "${aws_instance.Cmosetick-Nano.public_ip}"
-        timeout = "2m"
-        key_file = "files/cmosetick-terraform"
-    	}
-      	script = "files/doit.sh"
+  ami = "${lookup (var.my_amis,var.region) }"
+  instance_type = "t2.nano"
+  key_name = "${var.key_name}" # ssh key required for terraform provisioner
+  subnet_id = "${aws_subnet.Cmosetick-Public.id}"
+	#subnet_id = "${var.my_subnet}"
+  availability_zone = "${var.my_availability_zone}"
+	# see variables.tf to control how many instances
+  count = "${var.count}"
+	#user_data = "${element(template_file.host_init.*.rendered, count.index)}"
+  security_groups = ["${aws_security_group.Cmosetick-Public.id}"]
+  # public ip is optional in this scenario, useful for testing s
+  associate_public_ip_address = "True"
+	user_data = "${file("cloud-config/install.yml")}"
+		tags {
+    Name = "${format("Cmosetick-Nano-%03d", count.index + 1)}"
     }
 }
