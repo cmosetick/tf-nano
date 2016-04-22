05620b583f707de36b2c689bbd10fc3c05fe1978
Chris Mosetick <cmosetick@gmail.com>
2016-04-21 21:27:17 -0700

    remove doit.sh script, replaced with cloud-config

--- files/doit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo cp /home/ubuntu/distelli.yml /etc/distelli.yml
-# wget must have --quiet switch
-# curl does not need -s switch 
-wget --quiet https://www.distelli.com/download/client
-chmod +x client
-./client
-sudo distelli agent install -conf /etc/distelli.yml
