1c805b0bd0bda3f9cb3e64ca2a4b00268bed61ce
Chris Mosetick <cmosetick@gmail.com>
2016-04-21 21:13:05 -0700

    remove doit.sh script

--- files/doit.sh
+++ files/doit.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 sudo cp /home/ubuntu/distelli.yml /etc/distelli.yml
-curl -O https://www.distelli.com/download/client
+# wget must have --quiet switch
+# curl does not need -s switch 
+wget --quiet https://www.distelli.com/download/client
 chmod +x client
 ./client
 sudo distelli agent install -conf /etc/distelli.yml
